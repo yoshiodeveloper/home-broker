ccf95c10732ae8b5789ad27334e0782dde94315b
Yoshio Iwamoto <yoshiodeveloper@gmail.com>
2020-09-21 12:02:03 -0300

    Fix on postgres test.

--- core/implem/postgresql/postgresql_test.go
+++ core/implem/postgresql/postgresql_test.go
@@ -2,13 +2,13 @@ package postgresql_test
 
 import (
 	"home-broker/core/implem/postgresql"
-	testpostgresql "home-broker/test/postgresql"
+	postgresqltests "home-broker/tests/postgresql"
 	"reflect"
 	"testing"
 )
 
 func TestNewDB(t *testing.T) {
-	expected := testpostgresql.GetDB()
+	expected := postgresqltests.GetDB()
 	db := postgresql.NewDB(expected.Host, expected.Port, expected.User, expected.Password, expected.DBName)
 	if !reflect.DeepEqual(db, expected) {
 		t.Errorf("received %v, expected %v", db, &expected)
